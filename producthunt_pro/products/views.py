a10a7fbac329e8e54a185e2390cbcabea4908c08
ravin-d-27 <ravin.d3107@outlook.com>
2023-10-01 15:52:04 +0530

    Added Python History

--- producthunt_pro/products/views.py
+++ producthunt_pro/products/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'products\home.html')
+    return render(request,'products/home.html')
 
 def basicpy(request):
     return render(request,'products/basicpython.html')
 
 def installpy(request):
     return render(request,'products/installation.html')
+
+def aboutpython(request):
+    return render(request, 'products/Python_Intro.html')
