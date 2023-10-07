e79a44e9eb0c640e3a4382c4bb3867380d84fbf8
ravin-d-27 <ravin.d3107@outlook.com>
2023-10-07 11:21:52 +0530

    Checking settings.py

--- producthunt_pro/producthunt/settings.py
+++ producthunt_pro/producthunt/settings.py
@@ -74,12 +74,11 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
