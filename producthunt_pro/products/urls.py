19e3526eb3d8729a54c1de01a557492087bac8fc
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-10-16 19:36:20 +0530

    fix: added variables page in urls and routes

--- producthunt_pro/products/urls.py
+++ producthunt_pro/products/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('installpy/',views.installpy,name='installpy'),
     path('aboutpython/',views.aboutpython,name='aboutpython'),
     path('keywords/',views.keywords, name='keywords'),
+    path('variables/', views.variables, name='variables'),
     path('datatypes/',views.datatypes, name='datatypes'),
     path('sets/',views.sets, name='sets'),
     path('statements/',views.statements, name='statements'),
